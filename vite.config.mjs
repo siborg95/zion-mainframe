29a41af38fd2e8ed1d068ce2c4e4ea90a4256e12
Your Name <youremail@example.com>
2025-08-09 21:28:40 +0100

    Fix allowedHosts placement for Railway

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,13 +5,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: true, // 0.0.0.0
-    allowedHosts: ['oracle-front-end-production.up.railway.app'],
+    host: true,
+    allowedHosts: ['oracle-front-end-production.up.railway.app']
   },
   preview: {
-    host: true, // 0.0.0.0
-    port: process.env.PORT ? Number(process.env.PORT) : 4173,
-    strictPort: true,
-    allowedHosts: ['oracle-front-end-production.up.railway.app'],
-  },
+    host: true,
+    allowedHosts: ['oracle-front-end-production.up.railway.app']
+  }
 })
+
