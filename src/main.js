d697ce82cfab055ac356c1b5dd1325a80d542219
Your Name <youremail@example.com>
2025-08-10 14:12:27 +0100

    Force SPA index (BUILD v7) and boot log

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 ﻿import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+console.log("SPA BOOT v7");     // you should see this in DevTools Console
 createApp(App).use(router).mount("#app");
